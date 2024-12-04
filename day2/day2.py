3aba80e2fb6d6ad5e0856d680a20db4dc19789dc
Mark White <mwhite@redhat.com>
2024-12-04 12:27:19 -0500

    refactored to use recursion

--- day2/day2.py
+++ day2/day2.py
@@ -4,46 +4,46 @@ import re
 # Advent of Code - Day 2
 # https://adventofcode.com/2024/day/2
 
-def isReportSafe(reportList):
-    isSecond = False
-    isIncreasing = False
-    prior = -1
-    for level in reportList:
-        if level == "":
-            continue
-        level = int(level)
-        if prior == -1:
-            prior = level
-            isSecond = True
-            continue
-        if isSecond:
-            if level > prior: # this level is higher than the last, the reprot must always increase now
-                isIncreasing = True
-                isSecond = False
-            if level < prior: # this level is less than the last one, the report must now always decrease
-                isIncreasing = False # this was already set, resetting just to be safe
-                isSecond = False
-        if level == prior: # can't be equal adjacent reports
-            return False
-        # check the math first 
-        diff = abs(prior - level)
-        if diff < 1 or diff > 3:
-            return False # too far out of range, not safe
-        if isIncreasing and prior > level:
-            return False # prior is greater than current level, meaning we're now decreasing, unsafe
-        if not isIncreasing and prior < level:
-            return False # should be decreasing but prior is less than current report, meaning we're increasing, unsafe
-        # made it through the test, reset prior, move on
-        prior = level
-    return True
+def isReportSafeRecursive(reportList):
+    return checkLevel(reportList, -1, False, 0)
+
+def checkLevel(levelsToBeChecked, priorLevel, isIncreasing, cardinal):
+    testLevel = levelsToBeChecked.pop(0)
+    if cardinal >= 2 and validDifferences(priorLevel, testLevel): # third level or beyond, isIncreasing should be valid, priorLevel should be valid
+        if (isIncreasing and priorLevel < testLevel) or (not isIncreasing and priorLevel > testLevel):
+            return len(levelsToBeChecked) == 0 or checkLevel(levelsToBeChecked, testLevel, isIncreasing, cardinal + 1)
+    elif cardinal == 1 and validDifferences(priorLevel, testLevel): # this is the second item, special item to determine if we're increasing or decreasing
+        if testLevel > priorLevel:
+            return len(levelsToBeChecked) == 0 or checkLevel(levelsToBeChecked, testLevel, True, cardinal + 1)
+        else:
+            return len(levelsToBeChecked) == 0 or checkLevel(levelsToBeChecked, testLevel, False, cardinal + 1)
+    elif cardinal == 0: #cardinal = 0 here
+        return len(levelsToBeChecked) == 0 or checkLevel(levelsToBeChecked, testLevel, False, 1)
+    return False
+
+def validDifferences(priorLevel, testLevel):
+    diff = abs(priorLevel - testLevel)
+    return diff >= 1 and diff <= 3
+
 
 
 with open('day2/input.txt', 'r') as file:
     safeCount = 0
     for line in file:
         args = re.split(r"\s+", line)
-        if isReportSafe(args):
+        levels = []
+        
+        # convert string list into int list
+        for arg in args:
+            if arg == "":
+                continue #skip over blanks
+            levels.append(int(arg))
+        
+        # check if the report levels are safe
+        if isReportSafeRecursive(list(levels)):
             safeCount += 1
-        # print("line:\"" + line + "\"\targs:")
+            print("good list recur: " + str(levels))
+        else:
+            print("bad list recur: " + str(levels))
 
-    print("Safe Reports: " + str(safeCount))
+    print("Safe Reports (Recursive): " + str(safeCount))
